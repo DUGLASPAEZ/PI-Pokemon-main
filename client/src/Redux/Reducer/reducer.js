ad31b2449bff785d7c459f9442d080a53f467598
DUGLASPAEZ <paez18730@gmail.com>
2023-09-12 23:06:19 -0600

    faltan css en botones del filtradp

--- client/src/Redux/Reducer/reducer.js
+++ client/src/Redux/Reducer/reducer.js
@@ -7,7 +7,7 @@ import {
   ORDER_ORIGIN,
   ORDER_TYPES,
   FAILURE,
-  SET_CURRENT_PAGE, // Nueva acción para cambiar la página actual
+
 } from "../Actions/action-types";
 
 const initialState = {
@@ -42,46 +42,79 @@ const rootReducer = (state = initialState, action) => {
         PokemonName: action.payload,
       };
 
-    case ORDER_NAME:
-      // Lógica de ordenamiento por nombre...
-      break;
-
-    case ORDER_ATTACK:
-      // Lógica de ordenamiento por ataque...
-      break;
-
-    case ORDER_ORIGIN:
-      // Lógica de ordenamiento por origen...
-      break;
-
-    case ORDER_TYPES:
-      // Lógica de ordenamiento por tipos...
-      break;
-
-    case FAILURE:
-      if (action.payload === "Err") {
-        return {
-          ...state,
-          Failure: "",
-        };
-      } else {
-        return {
-          ...state,
-          Failure: action.payload,
-        };
+      case ORDER_NAME:
+        if (action.payload === 'All') {
+          return { ...state, OrderName: state.Pokemons };
+        } else if (action.payload === 'Az') {
+          const sortedPokemons = [...state.Pokemons].sort((a, b) => a.name.localeCompare(b.name, 'en', { sensitivity: 'base' }));
+          return { ...state, OrderName: sortedPokemons };
+        } else if (action.payload === 'Za') {
+          const sortedPokemonsZA = [...state.Pokemons].sort((a, b) => b.name.localeCompare(a.name, 'en', { sensitivity: 'base' }));
+          return { ...state, OrderName: sortedPokemonsZA };
+        }
+        break;
+            
+          case ORDER_ATTACK:
+              if(action.payload==='All'){
+                  return {...state, OrderAttack: state.Pokemons}
+              }
+              else if (action.payload === 'Max') {
+                  const sortedPokemonsMax = [...state.Pokemons].sort((a, b) => b.attack - a.attack);
+                  return { ...state, OrderAttack: sortedPokemonsMax };
+                }
+                else if (action.payload === 'Min') {
+                  const sortedPokemonsMin = [...state.Pokemons].sort((a, b) => a.attack - b.attack);
+                  return { ...state, OrderAttack: sortedPokemonsMin };
+                }
+               break;
+                case ORDER_ORIGIN:
+                  if(action.payload === 'All'){
+                      return { ...state, OrderOrigin: state.Pokemons }
+                  }
+                  else if (action.payload === 'Created'){
+                     const PokemonsDb= state.Pokemons.filter((e) => e.createdInDb)
+                     if(PokemonsDb.length===0){
+                      return {
+                        ...state, Failure: 'No Pokemons Here'
+                      }
+                     }
+                     return { ...state, OrderOrigin: PokemonsDb}
+                  }
+                  else if(action.payload==='Api'){
+                      const PokemonsApi= state.Pokemons.filter((e)=>!e.createdInDb)
+                      return { ...state, OrderOrigin: PokemonsApi}
+                  }
+                  break;
+      
+                  case ORDER_TYPES:
+                  let filterType;
+                  if (action.payload === "All") {
+                 filterType = state.Pokemons;
+                 } else {
+                    filterType = state.Pokemons.filter((e) =>
+                 e.types.includes(action.payload)
+                 );
+                }
+                if(filterType.length===0) return {
+                  ...state, Failure: 'No Pokemons Here'
+                }
+                   return {
+                 ...state,
+                  OrderTypes: filterType,
+                   }
+      
+                   case FAILURE: if (action.payload==='Err'){
+                    return {...state, Failure:''}
+                   }
+                   else return {
+                    ...state, Failure: action.payload
+                   }
+              
+          default: return {
+              ...state
+          }
       }
-
-    case SET_CURRENT_PAGE:
-      return {
-        ...state,
-        currentPage: action.payload, // Actualiza la página actual
-      };
-
-    default:
-      return {
-        ...state,
-      };
-  }
-};
+      
+      }
 
 export default rootReducer;
--- client/src/Views/HomePage/Home.jsx
+++ client/src/Views/HomePage/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { allPokemons } from '../../Redux/Actions/action';
+import { allPokemons, allTypes, orderName, orderAttack, orderOrigin, orderTypes } from '../../Redux/Actions/action';
 import Cards from '../../Components/CardsPage/Cards';
 import Search from '../../Components/SearchPage/Search';
 import NavBar from '../../Components/NavBarPage/NavBar';
@@ -12,20 +12,30 @@ function Home() {
 
   // Estados desde Redux
   const pokemons = useSelector((state) => state.Pokemons);
+  const Error = useSelector((state) => state.Failure);
 
-
-  // Estado local para la paginación
+  // Estado local para almacenar los datos actuales a mostrar en la página
+  const [currentData, setCurrentData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-
-  // Calcular el índice de inicio y final de la página actual
-  const startIndex = (currentPage - 1) * POKEMON_PER_PAGE;
-  const endIndex = startIndex + POKEMON_PER_PAGE;
-  const paginatedPokemons = pokemons.slice(startIndex, endIndex);
+  const [error, setError] = useState('');
+  const [filter, setFilter] = useState(''); // Estado para el filtro actual
+  const [showTypeFilter, setShowTypeFilter] = useState(false); // Estado para mostrar/ocultar el filtro de tipo
 
   useEffect(() => {
     // Cargar todos los Pokémon cuando el componente se monta
     dispatch(allPokemons());
+    dispatch(allTypes());
   }, [dispatch]);
+
+  // Efecto para actualizar 'currentData' cuando cambian los datos de Redux
+  useEffect(() => {
+    setCurrentData(pokemons);
+  }, [pokemons]);
+
+  // Efecto para manejar errores
+  useEffect(() => {
+    setError(Error);
+  }, [Error]);
 
   // Función para cambiar de página
   const handlePageChange = (newPage) => {
@@ -33,23 +43,114 @@ function Home() {
     const nextPage = currentPage + newPage;
 
     // Asegurarse de que la nueva página esté dentro de los límites válidos
-    if (nextPage >= 1 && nextPage <= Math.ceil(pokemons.length / POKEMON_PER_PAGE)) {
+    if (nextPage >= 1 && nextPage <= Math.ceil(currentData.length / POKEMON_PER_PAGE)) {
       setCurrentPage(nextPage);
     }
   };
+
+  // Función para aplicar el filtro seleccionado
+  const applyFilter = (filterName) => {
+    setFilter(filterName);
+    setShowTypeFilter(false); // Ocultar el filtro de tipo al aplicar otro filtro
+
+    switch (filterName) {
+      case 'NameAsc':
+        dispatch(orderName('Az'));
+        break;
+      case 'NameDesc':
+        dispatch(orderName('Za'));
+        break;
+      case 'AttackAsc':
+        dispatch(orderAttack('Min'));
+        break;
+      case 'AttackDesc':
+        dispatch(orderAttack('Max'));
+        break;
+      case 'OriginDb':
+        dispatch(orderOrigin('Created'));
+        break;
+      case 'OriginApi':
+        dispatch(orderOrigin('Api'));
+        break;
+      case 'Todos':
+        setCurrentData(pokemons);
+        break;
+      default:
+        // Filtro por tipo
+        dispatch(orderTypes(filterName));
+        break;
+    }
+  };
+
+  // Filtrar los Pokémon en base al filtro actual
+  const filteredPokemons = () => {
+    if (filter.startsWith('Type:')) {
+      const type = filter.split(':')[1];
+      return currentData.filter((pokemon) => pokemon.types.includes(type));
+    }
+
+    switch (filter) {
+      case 'NameAsc':
+        return currentData.slice().sort((a, b) => a.name.localeCompare(b.name));
+      case 'NameDesc':
+        return currentData.slice().sort((a, b) => b.name.localeCompare(a.name));
+      case 'AttackAsc':
+        return currentData.slice().sort((a, b) => a.attack - b.attack);
+      case 'AttackDesc':
+        return currentData.slice().sort((a, b) => b.attack - a.attack);
+      case 'OriginDb':
+        return currentData.filter((pokemon) => pokemon.createdInDb);
+      case 'OriginApi':
+        return currentData.filter((pokemon) => !pokemon.createdInDb);
+      case 'Todos':
+        return currentData;
+      default:
+        return currentData;
+    }
+  };
+
+  // Obtener todos los tipos disponibles
+  const availableTypes = [...new Set(pokemons.flatMap((pokemon) => pokemon.types))];
+
+  // Calcular el índice de inicio y final de la página actual
+  const startIndex = (currentPage - 1) * POKEMON_PER_PAGE;
+  const endIndex = startIndex + POKEMON_PER_PAGE;
 
   return (
     <div>
       <NavBar />
       <div className="imgcont">
         <Search />
-        <Cards Pokemons={paginatedPokemons} />
+        {/* Botones de filtrado */}
+        <div className="filter-buttons">
+          <button onClick={() => applyFilter('NameAsc')}>Nombre (A-Z)</button>
+          <button onClick={() => applyFilter('NameDesc')}>Nombre (Z-A)</button>
+          <button onClick={() => applyFilter('AttackAsc')}>Ataque (Menor a Mayor)</button>
+          <button onClick={() => applyFilter('AttackDesc')}>Ataque (Mayor a Menor)</button>
+          <button onClick={() => applyFilter('OriginDb')}>Origen (DB)</button>
+          <button onClick={() => applyFilter('OriginApi')}>Origen (API)</button>
+          <button onClick={() => applyFilter('Todos')}>Todos</button>
+          {/* Botón para mostrar/ocultar filtro de tipo */}
+          <button onClick={() => setShowTypeFilter(!showTypeFilter)}>Tipo</button>
+          {showTypeFilter && (
+            <div className="type-filter">
+              {availableTypes.map((type) => (
+                <button key={`Type:${type}`} onClick={() => applyFilter(`Type:${type}`)}>
+                  Tipo: {type}
+                </button>
+              ))}
+            </div>
+          )}
+        </div>
+        <Cards Pokemons={filteredPokemons().slice(startIndex, endIndex)} />
         {/* Botones de paginación */}
         <div className="pagination">
-          <button  className='buttonpaginate' onClick={() => handlePageChange(-1)}>Anterior</button>
+          <button className='buttonpaginate' onClick={() => handlePageChange(-1)}>Anterior</button>
           <span className='text'>Página {currentPage}</span>
           <button className='buttonpaginate' onClick={() => handlePageChange(1)}>Siguiente</button>
         </div>
+        {/* Mostrar el error si está presente */}
+        {error && <div className="error">{error}</div>}
       </div>
     </div>
   );
